7c935cbd04ab6d4898e2afe8c85abeb9fde79b29
Letki <502532346@qq.com>
2018-11-23 16:12:42 +0800

    version2 upload

--- 小程序/app.js
+++ 小程序/app.js
@@ -1,197 +1,193 @@
 //app.js
-var server_url= 'http://localhost:8080/CampusCard';
-var app=getApp;
+var serverUrl = require('pages/api/serverUrl.js')
+
 App({
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
   },
-  //获取用户信息方法弹窗
-  getUserInfo: function (cb) {
-    var _this = this;
-    //获取微信用户信息
-    wx.getUserInfo({
-      success: function (res) {
-        typeof cb == "function" && cb(res);
-      },
-      fail: function (res) {
-        //拒绝授权后的错误提示
-        _this.showErrorModal('拒绝授权将导致无法关联学校帐号并影响使用，请重新打开广金校园卡再点击允许授权！', '授权失败');
-        _this.g_status = '未授权';
-      }
-    });
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+
   },
-  saveCaChe:function(key,value){
-    if(!key||!value){return;}//key和value为空时
-    var _this = this;
-    _this.caChe[key]=value;//存进Cache中以便后面直接使用
-    wx.setStorage({
-      key: key,
-      data: value
-    });
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    
   },
-  //清楚某key缓存
-  removeCache: function (key) {
-    if (!key) { return; }
+  getUserInfo: function (fc) {
     var _this = this;
-    _this.cache[key] = '';
-    wx.removeStorage({
-      key: key
-    });
-  },
-  //错误信息封装
-  showErrorModal: function (content, title) {
-    wx.showModal({
-      title: title || '加载失败',
-      content: content || '未知错误',
-      showCancel: false
-    });
-  },
-  //获取用户数据去判断是否绑定
-  getUser: function (response) {
-    var _this = this;
-    wx.showNavigationBarLoading();
-    wx.login({
-      success: function (res) {
-        if (res.code) {
-          //调用函数获取微信用户信息
-          _this.getUserInfo(function (info) {
-            _this._user.wx = info.userInfo;
-            if (!info.encryptedData || !info.iv) {
-              _this.g_status = '无关联AppID';
-              typeof response == "function" && response(_this.g_status);
-              return;
-            }
-            console.log(typeof response == "function");
-            wx.showLoading({
-              title: '获取数据中',
-              mask: true
-            })
-            //发送code与微信用户信息，获取学生数据
-            wx.request({
-              method: 'POST',
-              url: _this.server_url + '/Login',//先login在微信注册
-              data: {
-                code: res.code,
-                encryptedData: info.encryptedData,
-                iv: info.iv
-              },
-              header: {
-                'content-type': 'application/x-www-form-urlencoded'
-              },
-              success: function (reponse) {
-                if (reponse.data.errmsg = "0") {
-                  _this._user.openid = reponse.data.unionId;
-                  _this._user.status = reponse.data.status;
-                  wx.request({
-                    method: 'POST',
-                    url: _this.server_url + '/UserBinding',//判断是否已经绑定
-                    data: {
-                      unionId: reponse.data.unionId,
-                    },
-                    header: {
-                      'content-type': 'application/x-www-form-urlencoded'
-                    },
-                    success: function (res) {
-                      if (res.data.Binding=="0"){
-                        console.log("nobinding");
-                        _this._user.binding = false;
-                        wx.navigateTo({
-                          url: '../login/login',//跳转到绑定页面
-                        })
-                        return;
-                      }
-                       else{
-                        typeof response == "function" && response(reponse.data.unionId);
-                       }
-                    },
-                    fail: function (res) {
-                    }
-                  })
-                }
-              },
-              fail: function (res) {
-                wx.showModal({
-                  title: '错误',
-                  content: '连接服务器失败',
-                  success: function (res) {
-                    if (res.confirm) {
-                      console.log('用户点击确定')
-                    } else if (res.cancel) {
-                      console.log('用户点击取消')
-                    }
-                  }
-                })
-              },
-              complete: function () {
-                wx.hideLoading();
-                wx.hideNavigationBarLoading();
-              }
-            });
-          });
+    return new Promise(function (resolve, reject) {
+      wx.showLoading({
+        title: '正在登录..',
+      })
+      wx.getSetting({
+        success: function (res) {
+          if (typeof res.authSetting["scope.userInfo"] == "undefined" || res.authSetting["scope.userInfo"] == false) {
+            //没有授权
+            _this.hasAuthorize = false;
+            reject('noAuthorise')
+            wx.hideLoading()
+            return false
+          } else {
+            //已经授权
+            _this.hasAuthorize = true;
+            resolve(res);
+          }
         }
-      }
+      })
     })
   },
-  //获取饭卡数据
-  getCardDetail: function (cb,page,unionId){
-    var _this=this;
-      _this._user.binding = true;
-      //获取数据
+  loginGetOpenid: function () {
+    var _this = this;
+    return new Promise(function (resolve, reject) {
+      wx.login({
+        success: function (res) {
+          wx.getUserInfo({
+            success: function (info) {
+              _this.wxInfo = info.userInfo
+              wx.request({
+                method: 'POST',
+                url: serverUrl.url + '/login',
+                data: {
+                  code: res.code,
+                  encryptedData: info.encryptedData,
+                  iv: info.iv
+                },
+                header: {
+                  'content-type': 'application/x-www-form-urlencoded'
+                },
+                success: function (response) {
+                  wx.hideLoading();
+                  if (response.statusCode === 200){
+                    if (response.data.bind == "1") {
+                      _this._user.binding = true;
+                      _this._user.openid = response.data.openid;
+                      //openid存入缓存
+                      wx.setStorage({
+                        key: 'okey',
+                        data: response.data.openid,
+                      })
+                      wx.setStorage({
+                        key: 'wxInfo',
+                        data: _this.wxInfo,
+                      })
+                      // 存入时间
+                      wx.setStorage({
+                        key: 'okeyTime',
+                        data: new Date().getTime(),
+                      })
+                      // 绑定状态存入缓存
+                      wx.setStorage({
+                        key: 'bind',
+                        data: true,
+                      })
+                      //获取梗概数据
+                      resolve(response)
+                    } else {
+                      //未绑定
+                      _this._user.binding = false;
+                      _this._user.openid = response.data.openid;
+                      wx.setStorage({
+                        key: 'bind',
+                        data: false,
+                      })
+                      reject("nobind")
+                    }
+                  }
+                  else {
+                    wx.showModal({
+                      title: '错误',
+                      content: '服务器连接失败,请检查网络连接',
+                      confirmText: '再试一次',
+                      success: function (res) {
+                        if (res.confirm) {
+                          // 重新加载
+                          reject("login 404")
+                        }
+                      }
+                    })
+                    
+                  }
+                },
+                fail: function () {
+                  wx.showModal({
+                    title: '错误',
+                    content: '服务器连接失败,请检查网络连接',
+                    confirmText: '再试一次',
+                    success: function (res) {
+                      if (res.confirm) {
+                        // 重新加载
+                        reject("LoginConnetFail")
+                      }
+                    }
+                  })
+                },
+                complete: function () {
+                  wx.hideLoading()
+                }
+              })
+            }
+          })
+        }
+      })
+    })
+  },
+  getUserDetail: function () {
+    var _this = this;
+    return new Promise(function (resolve, reject) {
       wx.request({
         method: 'POST',
-        url: _this.server_url + '/DataInfo',//获取明细
+        url: serverUrl.url + '/detail', //获取明细
         data: {
-          unionId: unionId,
-          page: page
+          openid: _this._user.openid,
         },
         header: {
           'content-type': 'application/x-www-form-urlencoded'
         },
-        success: function (detail) {
-          _this.detail_10 = detail.data.Info;
-          typeof cb == "function" && cb(detail.data.Info);
+        success: function (res) {
+          if (res.statusCode === 200){
+            wx.hideLoading()
+            _this.userInfo
+            resolve(res)
+          }
+          else {
+            reject('detail 404')
+          }
         }
       })
-  },
-  //获取饭卡信息
-  getUserDetail:function(cb,unionId){
-    var _this=this;
-    wx.request({
-      method: 'POST',
-      url: _this.server_url + '/Detail',//获取明细
-      data: {
-        unionId: unionId,
-      },
-      header: {
-        'content-type': 'application/x-www-form-urlencoded'
-      },
-      success: function (detail) {
-        console.log(detail.data.detail);
-        typeof cb == "function" && cb(detail.data.detail);
-      }
     })
   },
-  globalData: {
-    costData:{},
-    userInfo: null,
-    tapDetail: { cost: 0 }
+  navigateTo: function (e) {
+    console.log(e)
+    var url = e.currentTarget.dataset.url
+    wx.navigateTo({
+      url: url
+    })
   },
-  util: require('./utils/util'),
-  key: function (data) { return this.util.key(data) },
-  enCodeBase64: function (data) { return this.util.base64.encode(data) },
-  cache: {},
+  hasAuthorize: null,
   _user: {
-    openid:'',
-    //微信数据
-    wx: {},
-    //学生or老师数据
-    users: {}
+    binding: null,
+    openid: null
   },
-  detail_10:[],
-  server_url1:'http://localhost:8080/CampusCard',
-  server_url:'http://www.doline.site/CampusCard',
+  wxInfo: null,
+  changeBinding: false,
+  detail_10: [],
+  showError: function (title, msg) {
+    wx.showModal({
+      title: title,
+      content: msg,
+      showCancel: false,
+      confirmText: '真糟糕!'
+    })
+  },
+  // server_url1: 'http://localhost:8080/campus',
+  // server_url: 'https://www.funcard.top/campusNew',
 })
